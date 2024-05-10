696d67ad33b71757badcdfe480ff3702e56c33c7
Pablo Franco <pablofranco@yahoo.com>
2024-05-10 13:56:23 -0300

    Fixed quotes bug for backward python versions compatibility

--- src/service/http_service.py
+++ src/service/http_service.py
@@ -11,7 +11,7 @@ google_tbm_param = {
 
 def google_search(search_text, search_type):
     uri = 'https://www.google.com/search'
-    search_parameters = f'?q={search_text.replace(' ', '+')}'
+    search_parameters = f"?q={search_text.replace(' ', '+')}"
     tbm = f'&tbm={google_tbm_param[search_type]}' if google_tbm_param[search_type] else ''
     full_path = uri + search_parameters + tbm
     response = requests.get(full_path)
